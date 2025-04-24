54609a0203de404e7a3520f80404d4804e1e7c77
WillaG-E <willa.galipeaueldridge@maine.edu>
2025-04-24 10:47:28 -0400

    Add in comments for the requirements in MorseTester

---
+++ MorseTester.java
@@ -0,0 +1,14 @@
+public class MorseTester {
+    public static void main(String[] args) {
+        MorseTree morseTree = new MorseTree();
+        //Send MorseCode.txt to Tree
+
+        //Pre-Order of Tree
+
+        //Post-Order of Tree
+
+        //Translate English Text to Morse
+
+        //Translate Morse to English Text
+    }
+}
