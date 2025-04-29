c5e4596007f8b4cfec0a84a2efe2c02e45544873
WillaG-E <willa.galipeaueldridge@maine.edu>
2025-04-29 10:04:06 -0400

    Add in the hard-coded morsecode

--- MorseTree.java
+++ MorseTree.java
@@ -10,5 +10,30 @@ public class MorseTree {
 
     private void buildTree(){
         insert("e", new String[]{"o"});
+        insert("t", new String[]{"-"});
+        insert("i", new String[]{"o", "o"});
+        insert("a", new String[]{"o", "-"});
+        insert("n", new String[]{"-", "o"});
+        insert("m", new String[]{"-", "-"});
+        insert("s", new String[]{"o", "o", "o"});
+        insert("u", new String[]{"o", "o", "-"});
+        insert("r", new String[]{"o", "-", "o"});
+        insert("w", new String[]{"o", "-", "-"});
+        insert("d", new String[]{"-", "o", "o"});
+        insert("k", new String[]{"-", "o", "-"});
+        insert("g", new String[]{"-", "-", "o"});
+        insert("o", new String[]{"-", "-", "-"});
+        insert("h", new String[]{"o", "o", "o", "o"});
+        insert("v", new String[]{"o", "o", "o", "-"});
+        insert("f", new String[]{"o", "o", "-", "o"});
+        insert("l", new String[]{"o", "-", "o", "o"});
+        insert("p", new String[]{"o", "-", "-", "o"});
+        insert("j", new String[]{"o", "-", "-", "-"});
+        insert("b", new String[]{"-", "o", "o", "o"});
+        insert("x", new String[]{"-", "o", "o", "-"});
+        insert("c", new String[]{"-", "o", "-", "o"});
+        insert("y", new String[]{"-", "o", "-", "-"});
+        insert("z", new String[]{"-", "-", "o", "o"});
+        insert("q", new String[]{"-", "-", "o", "-"});
     }
 }
